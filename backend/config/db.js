2e6fcab1ccb854e4dd26823d6a0adc674c5924d3
sarthakchopde <sarthakchopde71@gmail.com>
2026-08-06 18:39:48 +0530

    Debug MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,15 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    console.log("==================================");
+    console.log("MongoDB Host:", conn.connection.host);
+    console.log("MongoDB Database:", conn.connection.name);
+    console.log("MongoDB ReadyState:", conn.connection.readyState);
+    console.log("==================================");
+
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(error);
     process.exit(1);
   }
 };
